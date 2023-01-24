b28e7177fd561e42968a9e2313ece6c8eb93f02c
Chase <chasevriend84@gmail.com>
2023-01-24 08:00:51 -0800

    Add files via upload
    finding difficulty in adding the "random" page to the application. Keep receiving "Error during template rendering"

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("add", views.add, name="add"),
+    path("random", views.random, name="random")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,35 @@
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
+from django.urls import reverse
 from . import util
 
+class NewEntryForm(forms.Form):
+    form = forms.CharField(label="New Entry")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def add(request):
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+            entry = form.cleaned_data["entry"]
+            request.session["entries"] += [entry]
+            return HttpResponseRedirect(reverse("encyclopedia:index"))
+
+        else:
+            return render(request, "encyclopedia/add.html", {
+                "form": form
+            })
+
+    return render(request, "encyclopedia/add.html", {
+        "form": NewEntryForm()
+    })
+
+def random(request):
+    return render(request, "encyclopedia/random.html", {
+        "random": random
+    })
